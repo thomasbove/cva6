6229eaa120f191395d0753ec699ce294b7e9af7d
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2021-11-09 18:31:55 +0100

    cache_ctrl: Fix typo
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- src/cache_subsystem/cache_ctrl.sv
+++ src/cache_subsystem/cache_ctrl.sv
@@ -250,7 +250,7 @@ module cache_ctrl import ariane_pkg::*; import std_cache_pkg::*; #(
                     // -------------------------
                     // Check for cache-ability
                     // -------------------------
-                    if (!is_inside_cacheable_regions(ArianeCfg, {{{64-riscv::PLEN}{1'b0}}, tag_o, {ICACHE_INDEX_WIDTH{1'b0}}})) begin
+                    if (!is_inside_cacheable_regions(ArianeCfg, {{{64-riscv::PLEN}{1'b0}}, tag_o, {DCACHE_INDEX_WIDTH{1'b0}}})) begin
                         mem_req_d.bypass = 1'b1;
                         state_d = WAIT_REFILL_GNT;
                     end
