a435b38bf233682f7edc201604129abb4c876c9b
aottaviano <aottaviano@iis.ee.ethz.ch>
2022-09-09 18:31:00 +0200

    tb: Add CLIC address space for tb and fpga simulations

--- tb/ariane_soc_pkg.sv
+++ tb/ariane_soc_pkg.sv
@@ -17,6 +17,10 @@ package ariane_soc;
   localparam int unsigned NumSources = 30;
   localparam int unsigned MaxPriority = 7;
 
+  // CLIC
+  localparam int unsigned NumInterruptSrc = 256;
+  localparam int unsigned CLICIntCtlBIts = 8;
+
   localparam NrSlaves = 2; // actually masters, but slaves on the crossbar
 
   // 4 is recommended by AXI standard, so lets stick to it, do not change
@@ -33,7 +37,8 @@ package ariane_soc;
     PLIC     = 6,
     CLINT    = 7,
     ROM      = 8,
-    Debug    = 9
+    Debug    = 9,
+    CLIC     = 10
   } axi_slaves_t;
 
   localparam NB_PERIPHERALS = Debug + 1;
@@ -50,6 +55,7 @@ package ariane_soc;
   localparam logic[63:0] GPIOLength     = 64'h1000;
   localparam logic[63:0] DRAMLength     = 64'h40000000; // 1GByte of DDR (split between two chips on Genesys2)
   localparam logic[63:0] SRAMLength     = 64'h1800000;  // 24 MByte of SRAM
+  localparam logic[63:0] CLICLength     = 64'h3FF_FFFF;
   // Instantiate AXI protocol checkers
   localparam bit GenProtocolChecker = 1'b0;
 
@@ -63,7 +69,8 @@ package ariane_soc;
     SPIBase      = 64'h2000_0000,
     EthernetBase = 64'h3000_0000,
     GPIOBase     = 64'h4000_0000,
-    DRAMBase     = 64'h8000_0000
+    DRAMBase     = 64'h8000_0000,
+    CLICBase     = 64'hD000_0000
   } soc_bus_start_t;
 
   localparam NrRegion = 1;
