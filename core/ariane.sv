fc0ed8e61cb80b7ef489e52a0ff63f12577dc7cd
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-03-21 16:05:24 +0100

    cva6: Expose sram_cfg_t param
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- core/ariane.sv
+++ core/ariane.sv
@@ -22,14 +22,21 @@ module ariane import ariane_pkg::*; #(
   parameter type axi_aw_chan_t = ariane_axi::aw_chan_t,
   parameter type axi_w_chan_t  = ariane_axi::w_chan_t,
   parameter type axi_req_t = ariane_axi::req_t,
-  parameter type axi_rsp_t = ariane_axi::resp_t
+  parameter type axi_rsp_t = ariane_axi::resp_t,
+  parameter type sram_cfg_t = logic
+
 ) (
   input  logic                         clk_i,
   input  logic                         rst_ni,
   // Core ID, Cluster ID and boot address are considered more or less static
   input  logic [riscv::VLEN-1:0]       boot_addr_i,  // reset boot address
   input  logic [riscv::XLEN-1:0]       hart_id_i,    // hart id in a multicore environment (reflected in a CSR)
-
+  // SRAM config
+  input sram_cfg_t                     sram_cfg_idata_i,
+  input sram_cfg_t                     sram_cfg_itag_i,
+  input sram_cfg_t                     sram_cfg_ddata_i,
+  input sram_cfg_t                     sram_cfg_dtag_i,
+  input sram_cfg_t                     sram_cfg_dvalid_dirty_i,
   // Interrupt inputs
   input  logic [1:0]                   irq_i,        // level sensitive IR lines, mip & sip (async)
   input  logic                         ipi_i,        // inter-processor interrupts (async)
@@ -68,12 +75,18 @@ module ariane import ariane_pkg::*; #(
     .axi_aw_chan_t (axi_aw_chan_t),
     .axi_w_chan_t (axi_w_chan_t),
     .axi_req_t (axi_req_t),
-    .axi_rsp_t (axi_rsp_t)
+    .axi_rsp_t (axi_rsp_t),
+    .sram_cfg_t (sram_cfg_t)
   ) i_cva6 (
     .clk_i                ( clk_i                     ),
     .rst_ni               ( rst_ni                    ),
     .boot_addr_i          ( boot_addr_i               ),
     .hart_id_i            ( hart_id_i                 ),
+    .sram_cfg_idata_i     ( sram_cfg_idata_i          ),
+    .sram_cfg_itag_i      ( sram_cfg_itag_i           ),
+    .sram_cfg_ddata_i     ( sram_cfg_ddata_i          ),
+    .sram_cfg_dtag_i      ( sram_cfg_dtag_i           ),
+    .sram_cfg_dvalid_dirty_i ( sram_cfg_dvalid_dirty_i ),
     .irq_i                ( irq_i                     ),
     .ipi_i                ( ipi_i                     ),
     .time_irq_i           ( time_irq_i                ),
